e7ac73b1c8f211c89399894498a25da7b2cc034f
TeaghanEveleigh <eteaghan@gmail.com>
2023-03-04 16:01:31 +1300

    Added enums to represent player speed status will later add the switch statement that will set the speed based on the player status also began constructing the first class of the two classes that will be in the game first of which being a goblin

---
+++ GoblinsAndRaiders.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+using namespace std;
+#define mapsize 10;
+
+
+
+
+
+//function generated gamespace
+void GenerateGameSpace(string MapNumbers);
+
+
+
+
+int main void(){
+
+//creates class goblin speed is fast when compared to all the other classes 
+class Goblins
+{
+private:
+    /* data */
+public:
+    Goblins(/* args */);
+    ~Goblins();
+};
+
+Goblins::Goblins(/* args */)
+{
+}
+
+Goblins::~Goblins()
+{
+}
+
+enum {
+
+    PlayerStatusRunning
+    PlayerStatusWalking
+    PlayerStatusCrouching
+    PlayerStatusBoosted
+
+}
+
+
+
+
+    system("pause");
+}
+
+void GenerateGameSpace(string MapNumbers,MapSize){
+    for(int i=0,i<mapsize,i++)
+    {
+        for(int k=0,k<mapsize,k++)
+            if(MapNubers==49)
+            {
+
+            }
+
+            else if() 
+            {
+
+            }
+
+            else if()
+            {
+
+            }
+
+            }
+    }
+
+
+
+}
